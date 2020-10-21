62e5c18e1cd221a9e635215c2591952789d850fd
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-10-21 15:35:51 -0400

    first push

---
+++ iphone12.js
@@ -0,0 +1,10 @@
+const iphone12 = {
+    image: 'path to where the image is located',
+    length: '11:28',
+    title: 'iPhone 12 Unboxing Experience + Magsafe ...',
+    username: 'Marques Brownlee',
+    views: '5.5M',
+    uploaded: 15
+}
+
+console.log(iphone12);
---
+++ star-wars.js
@@ -0,0 +1,7 @@
+const starwars = {
+    title: 'Star Wars',
+    jedi: 'Luke',
+    planet: 'Dark Star',
+}
+
+console.log(starwars);
